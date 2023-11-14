321cf46ac269a7241fee83f80c8f1e6e9085784c
Daniel Diaz <fer_cesar98@hotmail.com>
2023-11-14 09:06:00 -0500

    Segundo commit: Agregar nueva funcion de cargue de archivo directo sin el script de ingestión

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py
 
 from flask import Flask, request, jsonify
+from io import StringIO  # Para leer el contenido del archivo CSV desde la solicitud
+import csv
 
 app = Flask(__name__)
 
@@ -11,8 +13,25 @@ data_store = []
 def home():
     return "Operativo ····"
 
-@app.route('/load_data', methods=['POST'])
+@app.route('/load_data_file', methods=['POST'])
 def load_data():
+    try:
+        # Lee el archivo CSV desde la solicitud
+        csv_data = request.files['file'].read().decode('utf-8')
+        
+        # Parsea el contenido del CSV
+        csv_reader = csv.DictReader(StringIO(csv_data))
+        data = list(csv_reader)
+
+        # Almacena los datos
+        data_store.extend(data)
+
+        return jsonify({"message": "Datos cargados con éxito"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/load_data_json', methods=['POST'])
+def load_json():
     data = request.get_json().get('data')
     data_store.extend(data)
     return jsonify({"message": "CSV cargado con éxito"})
@@ -25,5 +44,10 @@ def query_data():
     print(result)
     return jsonify(result)
 
+# produccion
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080)
+
+# QA
+#if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=8080, debug=True)
